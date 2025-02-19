Sawmills/opentelemetry-collector-contrib

commit af6b17e83c1b0e95275fe356a2ae58859a8f9c98

Dependency: Update to Prometheus 0.300.* (#36873)
#### Description
Supersedes
https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/36642

This PR updates the prometheus/prometheus library in our go.mods to
0.300.* (which represents Prometheus 3.0).

It touches many go.mod files, but the Prometheus Receiver is the only
component heavily affected by breaking changes.

---------

Signed-off-by: Arthur Silva Sens <arthursens2005@gmail.com>

exporter/datadogexporter/examples/collector-metrics.yaml
@@ -4,6 +4,7 @@ receivers:
       scrape_configs:
       - job_name: 'otelcol'
         scrape_interval: 10s
+        fallback_scrape_protocol: "PrometheusText1.0.0"
         static_configs:
         - targets: ['0.0.0.0:8888']
 

exporter/datadogexporter/examples/ootb-ec2.yaml
@@ -39,6 +39,7 @@ receivers:
       scrape_configs:
       - job_name: 'otelcol'
         scrape_interval: 10s
+        fallback_scrape_protocol: "PrometheusText1.0.0"
         static_configs:
         - targets: ['0.0.0.0:8888']
 

receiver/prometheusreceiver/factory.go
@@ -6,6 +6,7 @@ package prometheusreceiver // import "github.com/open-telemetry/opentelemetry-co
 import (
 	"context"
 
+	"github.com/prometheus/common/model"
 	promconfig "github.com/prometheus/prometheus/config"
 	_ "github.com/prometheus/prometheus/discovery/install" // init() of this package registers service discovery impl.
 	"go.opentelemetry.io/collector/component"
@@ -35,6 +36,12 @@ var enableNativeHistogramsGate = featuregate.GlobalRegistry().MustRegister(
 
 // NewFactory creates a new Prometheus receiver factory.
 func NewFactory() receiver.Factory {
+	// Since Prometheus 3.0, the default validation scheme for metric names is UTF8.
+	// This includes ScrapeManager lib that is used by the Promethes receiver.
+	// We need to set the validation scheme to _something_ to avoid panics, and
+	// UTF8 is the default in Prometheus.
+	model.NameValidationScheme = model.UTF8Validation
+
 	return receiver.NewFactory(
 		metadata.Type,
 		createDefaultConfig,
@@ -56,6 +63,7 @@ func createMetricsReceiver(
 	nextConsumer consumer.Metrics,
 ) (receiver.Metrics, error) {
 	configWarnings(set.Logger, cfg.(*Config))
+	addDefaultFallbackScrapeProtocol(cfg.(*Config))
 	return newPrometheusReceiver(set, cfg.(*Config), nextConsumer), nil
 }
 
@@ -65,6 +73,14 @@ func configWarnings(logger *zap.Logger, cfg *Config) {
 			if rc.TargetLabel == "__name__" {
 				logger.Warn("metric renaming using metric_relabel_configs will result in unknown-typed metrics without a unit or description", zap.String("job", sc.JobName))
 			}
+		}
+	}
+}
+
+func addDefaultFallbackScrapeProtocol(cfg *Config) {
+	for _, sc := range cfg.PrometheusConfig.ScrapeConfigs {
+		if sc.ScrapeFallbackProtocol == "" {
+			sc.ScrapeFallbackProtocol = promconfig.PrometheusText1_0_0
 		}
 	}
 }

exporter/datadogexporter/integrationtest/integration_test_internal_metrics_config.yaml
@@ -11,6 +11,7 @@ receivers:
       scrape_configs:
         - job_name: 'otelcol'
           scrape_interval: 1s
+          fallback_scrape_protocol: "PrometheusText1.0.0"
           static_configs:
             - targets: [ '${env:PROM_SERVER}' ]
 

exporter/datadogexporter/integrationtest/integration_test_logs_config.yaml
@@ -11,6 +11,7 @@ receivers:
       scrape_configs:
         - job_name: 'otelcol'
           scrape_interval: 1s
+          fallback_scrape_protocol: "PrometheusText1.0.0"
           static_configs:
             - targets: [ '${env:PROM_SERVER}' ]
           metric_relabel_configs:

receiver/prometheusreceiver/factory_test.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	promconfig "github.com/prometheus/prometheus/config"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
@@ -61,4 +62,22 @@ func TestMultipleCreate(t *testing.T) {
 	require.NoError(t, err)
 	require.NoError(t, secondRcvr.Start(context.Background(), host))
 	require.NoError(t, secondRcvr.Shutdown(context.Background()))
+}
+
+func TestDefaultFallbackScrapeProtocol(t *testing.T) {
+	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config_fallback_scrape_protocol.yaml"))
+	require.NoError(t, err)
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+
+	sub, err := cm.Sub(component.NewIDWithName(metadata.Type, "").String())
+	require.NoError(t, err)
+	assert.NoError(t, sub.Unmarshal(cfg))
+
+	_, err = factory.CreateMetrics(context.Background(), receivertest.NewNopSettings(), cfg, consumertest.NewNop())
+	require.NoError(t, err)
+
+	// During receiver creation, scrapeconfig without fallback scrape protocol set, should be set to 'PrometheusText1.0.0'.
+	assert.Equal(t, promconfig.PrometheusText1_0_0, cfg.(*Config).PrometheusConfig.ScrapeConfigs[0].ScrapeFallbackProtocol)
+	assert.Equal(t, promconfig.OpenMetricsText1_0_0, cfg.(*Config).PrometheusConfig.ScrapeConfigs[1].ScrapeFallbackProtocol)
 }

receiver/prometheusreceiver/internal/staleness_end_to_end_test.go
@@ -109,6 +109,7 @@ receivers:
       scrape_configs:
         - job_name: 'test'
           scrape_interval: 100ms
+          fallback_scrape_protocol: "PrometheusText1.0.0"
           static_configs:
             - targets: [%q]
 

receiver/prometheusreceiver/metrics_receiver_labels_test.go
@@ -6,6 +6,8 @@ package prometheusreceiver
 import (
 	"testing"
 
+	"github.com/prometheus/common/model"
+	promconfig "github.com/prometheus/prometheus/config"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/model/relabel"
 	"github.com/stretchr/testify/require"
@@ -232,6 +234,10 @@ test_counter0{label1="value1",label2="value2"} 1
 `
 
 func TestLabelNameLimitConfig(t *testing.T) {
+	scheme := model.NameValidationScheme
+	model.NameValidationScheme = model.UTF8Validation
+	defer func() { model.NameValidationScheme = scheme }()
+
 	targets := []*testData{
 		{
 			name: "target1",
@@ -252,6 +258,7 @@ func TestLabelNameLimitConfig(t *testing.T) {
 	testComponent(t, targets, nil, func(cfg *PromConfig) {
 		// set label limit in scrape_config
 		for _, scrapeCfg := range cfg.ScrapeConfigs {
+			scrapeCfg.ScrapeFallbackProtocol = promconfig.PrometheusText1_0_0
 			scrapeCfg.LabelNameLengthLimit = 20
 		}
 	})

commit 077e2657941d556f55430e6cf419e31b5ade0d7b

[receiver/prometheus] Add fallback_scrape_protocol during config validation (#38018)
#### Description
During the release process, we noticed a breaking change in the
Prometheus receiver caused by
https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/36873.

In that PR, I tried adding a fallback scrape protocol by default
everytime the PrometheusReceiver was built, but it turned out that
config validation happened even before the component was created. And
the collector fails startup with invalid configuration

This PR moves the addition of scrape protocol to the validation step.

<!-- Issue number (e.g. #1234) or full URL to issue, if applicable. -->
#### Link to tracking issue
Fixes #37902

<!--Describe what testing was performed and which tests were added.-->
#### Testing

<!--Describe the documentation added.-->
#### Documentation

<!--Please delete paragraphs that you did not use before submitting.-->

.chloggen/prom-scrape-config-protocol.yaml
@@ -1,0 +1,27 @@
+# Use this changelog template to create an entry for release notes.
+
+# One of 'breaking', 'deprecation', 'new_component', 'enhancement', 'bug_fix'
+change_type: bug_fix
+
+# The name of the component, or a single word describing the area of concern, (e.g. filelogreceiver)
+component: receiver/prometheus
+
+# A brief description of the change.  Surround your text with quotes ("") if it needs to start with a backtick (`).
+note: Fix Collector failing to start up if Prometheus receiver is present in config without 'fallback_scrape_protocol'.
+
+# Mandatory: One or more tracking issues related to the change. You can use the PR number here if no issue exists.
+issues: [38018]
+
+# (Optional) One or more lines of additional information to render under the primary note.
+# These lines will be padded with 2 spaces and then inserted directly into the document.
+# Use pipe (|) for multiline entries.
+subtext:
+
+# If your change doesn't affect end users or the exported elements of any package,
+# you should instead start your pull request title with [chore] or use the "Skip Changelog" label.
+# Optional: The change log or logs in which this entry should be included.
+# e.g. '[user]' or '[user, api]'
+# Include 'user' if the change is relevant to end users.
+# Include 'api' if there is a change to a library API.
+# Default: '[user]'
+change_logs: [user]

exporter/datadogexporter/examples/collector-metrics.yaml
@@ -4,7 +4,6 @@ receivers:
       scrape_configs:
       - job_name: 'otelcol'
         scrape_interval: 10s
-        fallback_scrape_protocol: "PrometheusText1.0.0"
         static_configs:
         - targets: ['0.0.0.0:8888']
 

exporter/datadogexporter/examples/ootb-ec2.yaml
@@ -39,7 +39,6 @@ receivers:
       scrape_configs:
       - job_name: 'otelcol'
         scrape_interval: 10s
-        fallback_scrape_protocol: "PrometheusText1.0.0"
         static_configs:
         - targets: ['0.0.0.0:8888']
 

receiver/prometheusreceiver/config.go
@@ -103,6 +103,15 @@ func (cfg *PromConfig) Validate() error {
 	if err != nil {
 		return err
 	}
+	// Since Prometheus 3.0, the scrape manager started to fail scrapes that don't have proper
+	// Content-Type headers, but they provided an extra configuration option to fallback to the
+	// previous behavior. We need to make sure that this option is set for all scrape configs
+	// to avoid introducing a breaking change.
+	for _, sc := range scrapeConfigs {
+		if sc.ScrapeFallbackProtocol == "" {
+			sc.ScrapeFallbackProtocol = promconfig.PrometheusText0_0_4
+		}
+	}
 
 	for _, sc := range scrapeConfigs {
 		if err := validateHTTPClientConfig(&sc.HTTPClientConfig); err != nil {

receiver/prometheusreceiver/factory.go
@@ -63,7 +63,6 @@ func createMetricsReceiver(
 	nextConsumer consumer.Metrics,
 ) (receiver.Metrics, error) {
 	configWarnings(set.Logger, cfg.(*Config))
-	addDefaultFallbackScrapeProtocol(cfg.(*Config))
 	return newPrometheusReceiver(set, cfg.(*Config), nextConsumer), nil
 }
 
@@ -73,14 +72,6 @@ func configWarnings(logger *zap.Logger, cfg *Config) {
 			if rc.TargetLabel == "__name__" {
 				logger.Warn("metric renaming using metric_relabel_configs will result in unknown-typed metrics without a unit or description", zap.String("job", sc.JobName))
 			}
-		}
-	}
-}
-
-func addDefaultFallbackScrapeProtocol(cfg *Config) {
-	for _, sc := range cfg.PrometheusConfig.ScrapeConfigs {
-		if sc.ScrapeFallbackProtocol == "" {
-			sc.ScrapeFallbackProtocol = promconfig.PrometheusText1_0_0
 		}
 	}
 }

exporter/datadogexporter/integrationtest/integration_test_internal_metrics_config.yaml
@@ -11,7 +11,6 @@ receivers:
       scrape_configs:
         - job_name: 'otelcol'
           scrape_interval: 1s
-          fallback_scrape_protocol: "PrometheusText1.0.0"
           static_configs:
             - targets: [ '${env:PROM_SERVER}' ]
 

exporter/datadogexporter/integrationtest/integration_test_logs_config.yaml
@@ -11,7 +11,6 @@ receivers:
       scrape_configs:
         - job_name: 'otelcol'
           scrape_interval: 1s
-          fallback_scrape_protocol: "PrometheusText1.0.0"
           static_configs:
             - targets: [ '${env:PROM_SERVER}' ]
           metric_relabel_configs:

receiver/prometheusreceiver/factory_test.go
@@ -8,7 +8,6 @@ import (
 	"path/filepath"
 	"testing"
 
-	promconfig "github.com/prometheus/prometheus/config"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
@@ -62,22 +61,4 @@ func TestMultipleCreate(t *testing.T) {
 	require.NoError(t, err)
 	require.NoError(t, secondRcvr.Start(context.Background(), host))
 	require.NoError(t, secondRcvr.Shutdown(context.Background()))
-}
-
-func TestDefaultFallbackScrapeProtocol(t *testing.T) {
-	cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config_fallback_scrape_protocol.yaml"))
-	require.NoError(t, err)
-	factory := NewFactory()
-	cfg := factory.CreateDefaultConfig()
-
-	sub, err := cm.Sub(component.NewIDWithName(metadata.Type, "").String())
-	require.NoError(t, err)
-	assert.NoError(t, sub.Unmarshal(cfg))
-
-	_, err = factory.CreateMetrics(context.Background(), receivertest.NewNopSettings(), cfg, consumertest.NewNop())
-	require.NoError(t, err)
-
-	// During receiver creation, scrapeconfig without fallback scrape protocol set, should be set to 'PrometheusText1.0.0'.
-	assert.Equal(t, promconfig.PrometheusText1_0_0, cfg.(*Config).PrometheusConfig.ScrapeConfigs[0].ScrapeFallbackProtocol)
-	assert.Equal(t, promconfig.OpenMetricsText1_0_0, cfg.(*Config).PrometheusConfig.ScrapeConfigs[1].ScrapeFallbackProtocol)
 }

receiver/prometheusreceiver/internal/staleness_end_to_end_test.go
@@ -109,7 +109,6 @@ receivers:
       scrape_configs:
         - job_name: 'test'
           scrape_interval: 100ms
-          fallback_scrape_protocol: "PrometheusText1.0.0"
           static_configs:
             - targets: [%q]
 

receiver/prometheusreceiver/metrics_receiver_labels_test.go
@@ -7,7 +7,6 @@ import (
 	"testing"
 
 	"github.com/prometheus/common/model"
-	promconfig "github.com/prometheus/prometheus/config"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/model/relabel"
 	"github.com/stretchr/testify/require"
@@ -258,7 +257,6 @@ func TestLabelNameLimitConfig(t *testing.T) {
 	testComponent(t, targets, nil, func(cfg *PromConfig) {
 		// set label limit in scrape_config
 		for _, scrapeCfg := range cfg.ScrapeConfigs {
-			scrapeCfg.ScrapeFallbackProtocol = promconfig.PrometheusText1_0_0
 			scrapeCfg.LabelNameLengthLimit = 20
 		}
 	})

receiver/prometheusreceiver/testdata/config_target_allocator.yaml
@@ -15,10 +15,8 @@ prometheus/withScrape:
     scrape_configs:
       - job_name: 'demo'
         scrape_interval: 5s
-        fallback_scrape_protocol: "PrometheusText1.0.0"
 prometheus/withOnlyScrape:
   config:
     scrape_configs:
       - job_name: 'demo'
         scrape_interval: 5s
-        fallback_scrape_protocol: "PrometheusText1.0.0"

receiver/prometheusreceiver/testdata/invalid-config-prometheus-file-sd-config-json.yaml
@@ -3,6 +3,5 @@ prometheus:
     scrape_configs:
       - job_name: 'demo'
         scrape_interval: 5s
-        fallback_scrape_protocol: "PrometheusText1.0.0"
         file_sd_configs:
         - files: ["./testdata/sd-config-with-null-target-group.json"]

receiver/prometheusreceiver/testdata/invalid-config-prometheus-file-sd-config-yaml.yaml
@@ -3,6 +3,5 @@ prometheus:
     scrape_configs:
       - job_name: 'demo'
         scrape_interval: 5s
-        fallback_scrape_protocol: "PrometheusText1.0.0"
         file_sd_configs:
         - files: ["./testdata/sd-config-with-null-target-group.yaml"]

receiver/prometheusreceiver/testdata/invalid-config-prometheus-nonexistent-scrape-config.yaml
@@ -17,4 +17,3 @@ prometheus/withOnlyScrape:
     scrape_configs:
       - job_name: 'demo'
         scrape_interval: 5s
-        fallback_scrape_protocol: "PrometheusText1.0.0"

receiver/prometheusreceiver/testdata/invalid-config-prometheus-unsupported-features.yaml
@@ -5,7 +5,6 @@ prometheus:
     scrape_configs:
       - job_name: 'demo'
         scrape_interval: 5s
-        fallback_scrape_protocol: "PrometheusText1.0.0"
     remote_write:
       - url: "https://example.org/write"
 

receiver/prometheusreceiver/testdata/nonexistent-prometheus-sd-file-config.yaml
@@ -3,6 +3,5 @@ prometheus:
     scrape_configs:
       - job_name: 'test'
         scrape_interval: 5s
-        fallback_scrape_protocol: "PrometheusText1.0.0"
         file_sd_configs:
         - files: ["./testdata/nonexistent-sd-file.json"]