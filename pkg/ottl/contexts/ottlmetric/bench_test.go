10f3d339b5745fbd94366061e8d62a763c7e7635
Amir Jakoby <amirjak@gmail.com>
2025-12-29 00:04:36 +0200

    feat(ottl): fast metric metadata paths

---
+++ pkg/ottl/contexts/ottlmetric/bench_test.go
@@ -0,0 +1,89 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package ottlmetric
+
+import (
+	"context"
+	"testing"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.uber.org/zap"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl"
+)
+
+func newBenchMetricContext() TransformContext {
+	metric := pmetric.NewMetric()
+	metric.Metadata().PutInt("foo", 7)
+	return NewTransformContext(
+		metric,
+		pmetric.NewMetricSlice(),
+		pcommon.NewInstrumentationScope(),
+		pcommon.NewResource(),
+		pmetric.NewScopeMetrics(),
+		pmetric.NewResourceMetrics(),
+	)
+}
+
+func newBenchMetricParser(withVM bool) (ottl.Parser[TransformContext], error) {
+	options := []ottl.Option[TransformContext]{}
+	if withVM {
+		options = append(options, ottl.WithVMEnabled[TransformContext]())
+	}
+	return NewParser(
+		map[string]ottl.Factory[TransformContext]{},
+		component.TelemetrySettings{Logger: zap.NewNop()},
+		options...,
+	)
+}
+
+func BenchmarkOTTLMetricMetadataEq_Interpreter(b *testing.B) {
+	parser, err := newBenchMetricParser(false)
+	if err != nil {
+		b.Fatal(err)
+	}
+	cond, err := parser.ParseCondition(`metadata["foo"] == 7`)
+	if err != nil {
+		b.Fatal(err)
+	}
+	ctx := context.Background()
+	tCtx := newBenchMetricContext()
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ok, err := cond.Eval(ctx, tCtx)
+		if err != nil {
+			b.Fatal(err)
+		}
+		if !ok {
+			b.Fatal("expected true")
+		}
+	}
+}
+
+func BenchmarkOTTLMetricMetadataEq_VM(b *testing.B) {
+	parser, err := newBenchMetricParser(true)
+	if err != nil {
+		b.Fatal(err)
+	}
+	cond, err := parser.ParseCondition(`metadata["foo"] == 7`)
+	if err != nil {
+		b.Fatal(err)
+	}
+	ctx := context.Background()
+	tCtx := newBenchMetricContext()
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ok, err := cond.Eval(ctx, tCtx)
+		if err != nil {
+			b.Fatal(err)
+		}
+		if !ok {
+			b.Fatal("expected true")
+		}
+	}
+}
